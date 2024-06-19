a9125e9ef4ebf20bea736866238b15a2717da65a
SafarovHusan200 <Safarovhusan2000@gmail.com>
2024-06-20 00:32:23 +0500

    add modal(send message tg chanel), responsive menu

--- js/script.js
+++ js/script.js
@@ -1,14 +1,59 @@
 const openModal = document.querySelectorAll("#open_modal");
-const modalOver = document.getElementById("modal_back");
-const body = document.getElementsByTagName("body")[0];
-const modalClose = document.getElementById("modal_close");
-const scrolBtn = document.getElementById("scroll_btn");
+(modalOver = document.getElementById("modal_back")),
+  (body = document.getElementsByTagName("body")[0]),
+  (modalClose = document.getElementById("modal_close")),
+  (scrolBtn = document.getElementById("scroll_btn")),
+  (forms = document.querySelectorAll("form")),
+  (openMenu = document.getElementById("open_menu")),
+  (menu = document.getElementById("menu")),
+  (closeMenu = document.getElementById("close_menu"));
 
 // Open Modal
 openModal.forEach((btn) => {
   btn.addEventListener("click", () => {
     body.style.overflow = "hidden";
     modalOver.classList.remove("hide");
+  });
+});
+
+forms.forEach((form) => {
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(form);
+    let object = {};
+
+    formData.forEach((value, key) => {
+      object[key] = value;
+    });
+
+    const telegramTokenBot = "6480010312:AAGkY_tyO05EheM2aw7hdcWlJl9BcYyQ0mw",
+      chatId = "@custodian1_support"; // Bu yerga kanal ID-sini yozing
+
+    fetch(`https://api.telegram.org/bot${telegramTokenBot}/sendMessage`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        chat_id: chatId,
+        text: `Name: ${object.name}.\nPhone: ${object.phone}. \nEmail: ${object.email}. \nMessage: ${object.message}`,
+      }),
+    })
+      .then((response) => {
+        if (response.ok) {
+          closeModalFunction();
+          alert("Message sent successfully!");
+          form.reset();
+        } else {
+          return response.json().then((errorData) => {
+            throw new Error(errorData.description);
+          });
+        }
+      })
+      .catch((err) => {
+        console.error("Error sending message:", err);
+        alert("Failed to send message. Please try again later.");
+      })
+      .finally(() => console.log("Request completed"));
   });
 });
 
@@ -43,4 +88,18 @@ scrolBtn.addEventListener("click", () => {
     top: 0,
     behavior: "smooth",
   });
+});
+
+// Menu open/close
+openMenu.addEventListener("click", () => {
+  console.log("open menu");
+  menu.classList.toggle("active_menu");
+});
+
+document.addEventListener("scroll", () => {
+  menu.classList.remove("active_menu");
+});
+
+closeMenu.addEventListener("click", () => {
+  menu.classList.toggle("active_menu");
 });
